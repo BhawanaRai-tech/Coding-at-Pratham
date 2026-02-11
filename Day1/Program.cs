3d0c7cab70aee15c623e6f1c56e4717b4b5bc48f
Bhawana Rai <bhawanasambawa26@gmail.com>
2026-02-11 14:06:34 +0545

    Day1: add addition and subtraction

---
+++ Day1/Program.cs
@@ -0,0 +1,30 @@
+﻿class Program
+{
+
+    static int sum()
+    { 
+        Console.Write("Enter first number: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter second number: ");
+        int b = Convert.ToInt32(Console.ReadLine());   
+        int c = a + b; 
+        return c;
+    }
+
+    static int diff(int a, int b)
+    {
+        int d = a - b;
+        return d;
+    }
+   
+    public static void Main()
+    {
+        int result = sum();
+        Console.WriteLine("The sum of 2 numbers is " + result);
+
+        Console.WriteLine("The difference is: " + diff(400, 200));
+    }
+    
+    
+}
+
