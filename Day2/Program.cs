6b11a19cad88919998c659c06792dddd757821a0
Bhawana Rai <bhawanasambawa26@gmail.com>
2026-02-12 09:55:47 +0545

    Day2:Use get and set to access and set data

---
+++ Day2/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Solution s = new Solution();
+        Console.WriteLine("Enter your name: ");
+        string name = Console.ReadLine();
+        
+        s.SetName(name);
+        Console.WriteLine(s.GetName());
+    }
+}
+
+class Solution
+{
+    public string name { get; set; }
+
+    public void  SetName(String name)
+    {
+        this.name = name;
+    }
+
+    public string GetName()
+    {
+        return name;
+    }
+}
